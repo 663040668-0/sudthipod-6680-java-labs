c212b853eb85e3b78bcbe61790d1475cac3630b3
Sudthipod Wanaprom <sudthipod.w@kkumail.com>
2023-11-20 18:26:04 +0700

    Done : SimpleCalculator

---
+++ wanaprom/sudthipod/lab2/SimpleCalculator.java
@@ -0,0 +1,78 @@
+package wanaprom.sudthipod.lab2;
+
+/**
+ * The SimpleCalculator Program:
+ * This program accepts three arguments and calculate for the result.
+ * Input expected: <a> <b> <operator>
+ * where <a> and <b> are "integers"
+ * and <operator> is a Mathematic operator.
+ * Valid operators: ( '+', '-', 'x', '/' )
+ * 
+ * Expected output:
+ * <a><operator><b>=<result>
+ * 
+ * Exclusive operators and division by zero are the exceptions
+ * 
+ * Author : Sudthipod Wanaprom
+ * ID : 663040668-0
+ * Sec : 2
+ */
+
+// Import classes or packages
+import java.util.Scanner;
+
+// Main class
+public class SimpleCalculator {
+    // Main method
+    public static void main(String[] args) {
+        // Declare variables and constants
+        final byte ARGS_COUNT = 3;
+
+        // Initial the input receiver
+        Scanner sc = new Scanner(System.in);
+
+        // Receive user's input and split the string
+        System.out.print("Input: ");
+        String[] tokenizedInput = sc.nextLine().split(" ");
+
+        // Check input requirements
+        int args_detected = tokenizedInput.length;
+        if (args_detected == ARGS_COUNT) {
+            // Declare variables
+            int a = Integer.parseInt(tokenizedInput[0]);
+            int b = Integer.parseInt(tokenizedInput[1]);
+            String operator = tokenizedInput[2];
+
+            // Check the operator
+            switch (operator) {
+                case "+":
+                    System.out.println(a + operator + b + "=" + (a + b));
+                    break;
+
+                case "-":
+                    System.out.println(a + operator + b + "=" + (a - b));
+                    break;
+
+                case "x":
+                    System.out.println(a + operator + b + "=" + (a * b));
+                    break;
+
+                case "/":
+                    if (b != 0)
+                        System.out.println(a + operator + b + "=" + (a / b));
+                    else
+                        System.err.println("Error: Division by zero is not allowed.");
+                    break;
+
+                default:
+                    System.err.println("Error: Invalid operator. Please use '+', '-', 'x', or '/'.");
+                    break;
+            }
+        } else
+            System.err.println("Error: Invalid number of arguments. Please provide exactly three arguments. "
+                    + args_detected + " was detected.");
+
+        // Close the scanner
+        sc.close();
+    }
+}
